304f634f42489caeba0febe0ae689bad51124f14
Samuel Dowd <dowd95@gmail.com>
2022-01-25 13:03:48 -0700

    Initial Commit

---
+++ Mogami iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mogami iOS
+//
+//  Created by Samuel Dowd on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
