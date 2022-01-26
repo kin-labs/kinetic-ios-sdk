5d5654825f761e87876e2d4f247129f887b2e34d
Samuel Dowd <dowd95@gmail.com>
2022-01-25 17:14:01 -0700

    initial commit, added solana dep and some boilerplate

--- Mogami iOS/ViewController.swift
+++ Mogami iOS/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Solana
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let networkingRouter = NetworkingRouter(endpoint: .mainnetBetaSolana)
+        let accountStorage = InMemoryAccountStorage()
+        let solana = Solana(router: networkingRouter, accountStorage: accountStorage)
+
+        if let account = Account(network: .mainnetBeta) {
+            print(account.publicKey)
+            print(account.secretKey)
+            print(account.phrase.joined(separator: " "))
+        }
     }
 
 
 }
 
+class InMemoryAccountStorage: SolanaAccountStorage {
+
+    private var _account: Account?
+    func save(_ account: Account) -> Result<Void, Error> {
+        _account = account
+        return .success(())
+    }
+    var account: Result<Account, Error> {
+        if let account = _account {
+            return .success(account)
+        }
+        return .failure(SolanaError.unauthorized)
+    }
+    func clear() -> Result<Void, Error> {
+        _account = nil
+        return .success(())
+    }
+}
