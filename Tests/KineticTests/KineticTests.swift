7820dae7477a4b7af3b1684c3918c6728825cc91
Samuel Dowd <dowd95@gmail.com>
2022-10-18 19:03:14 -0500

    Added testing and wrote first test

--- Tests/KineticTests/KineticTests.swift
+++ Tests/KineticTests/KineticTests.swift
@@ -2,10 +2,22 @@ import XCTest
 @testable import Kinetic
 
 final class KineticTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(kinetic().text, "Hello, World!")
+    let TEST_APP_INDEX = 1
+    let TEST_APP_ENVIRONMENT = "devnet"
+//    let TEST_APP_ENDPOINT = "https://staging.kinetic.host"
+    let TEST_APP_ENDPOINT = "http://localhost:3000"
+    let TEST_SOLANA_RPC_NAME = "mainnet-beta"
+    let TEST_SOLANA_RPC_ENDPOINT = "https://api.mainnet-beta.solana.com/"
+
+    func testSdkSetup() async throws {
+        let sdk = try await KineticSdk.setup(
+            endpoint: TEST_APP_ENDPOINT,
+            environment: TEST_APP_ENVIRONMENT,
+            index: TEST_APP_INDEX
+        )
+
+        XCTAssertEqual(sdk.environment, TEST_APP_ENVIRONMENT)
+        XCTAssertEqual(sdk.endpoint, TEST_APP_ENDPOINT)
+        XCTAssertEqual(sdk.index, TEST_APP_INDEX)
     }
 }
